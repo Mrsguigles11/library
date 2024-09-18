fc43e4b510ef750f93964f7a40438f64cf1a4bba
Harry Woodgate <harry_woodgate11@hotmail.co.uk>
2024-09-18 18:34:48 +0100

    Create an example table in the HTML and a javascript function that will cycle through the library array and add the books to the table.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
-const myLibrary = [];
+const table = document.querySelector(".table");
+
+let myLibrary = [];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -12,8 +14,20 @@ function addBookToLibrary (book) {
     myLibrary.push(book);
 }
 
+
 const theHobbit = new Book("The Hobbit", "J.R.R Tolkein", "395", "No");
+const pathOfDestruction = new Book("Path of Destruction", "Drew Karpyshyn", "450", "Yes");
 
 addBookToLibrary(theHobbit);
+addBookToLibrary(pathOfDestruction);
 
-console.log(myLibrary);
+for (const book of myLibrary) {
+    let row = document.createElement("tr");
+    table.appendChild(row);
+    for (const key in book) {
+        let dataCell = document.createElement("td");
+        row.appendChild(dataCell);
+        dataCell.textContent = `${book[key]}`;
+    }
+}
+
